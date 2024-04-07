a9f4565e9dff718c660038835fb0fc7170295c58
Çağatay Çelimli <c.celimli@live.co.uk>
2024-04-07 22:10:45 +0300

    friendly message

---
+++ user-service/src/main/java/com/ecommerce/utilities/helper/friendlyMessage/FriendlyMessageCode.java
@@ -0,0 +1,11 @@
+package com.ecommerce.utilities.helper.friendlyMessage;
+
+/**
+ * Copyright (c) 2024
+ * All rights reserved.
+ *
+ * @author Çağatay Çelimli
+ */
+public interface FriendlyMessageCode {
+    int getFriendlyMesssageCode();
+}
