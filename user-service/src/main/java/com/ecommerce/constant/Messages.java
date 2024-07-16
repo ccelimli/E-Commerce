4b0c986b2fac34a0edc57139b53cd0be45911f40
Çağatay Çelimli <c.celimli@live.co.uk>
2024-07-17 01:28:21 +0300

    Address Service with RestResponse

--- user-service/src/main/java/com/ecommerce/constant/Messages.java
+++ user-service/src/main/java/com/ecommerce/constant/Messages.java
@@ -21,7 +21,10 @@ public enum Messages {
 
     //User
     USER_ADDED("User Added To Database."),
-    USER_DELETED("Removed User From Database");
+    USER_DELETED("Removed User From Database"),
+    USER_LISTED("User Listed."),
+    USERS_LISTED("Users Listed."),
+    USER_UPDATED("Updated Infos of User.");
 
     private final String message;
 
--- user-service/src/main/java/com/ecommerce/controller/contract/impl/UserControllerContractImpl.java
+++ user-service/src/main/java/com/ecommerce/controller/contract/impl/UserControllerContractImpl.java
@@ -12,7 +12,6 @@ import com.ecommerce.service.UserEntityService;
 import com.ecommerce.service.mapper.UserMapper;
 import com.ecommerce.utilities.exceptions.AlreadyExistsException;
 import com.ecommerce.utilities.exceptions.ItemNotFoundException;
-import com.ecommerce.utilities.exceptions.ServiceUnavailableException;
 import com.ecommerce.utilities.exceptions.UserIsNotActiveException;
 import com.ecommerce.utilities.general.entity.RestResponse;
 import com.ecommerce.utilities.helper.BusinessRules.RegularExpression;
@@ -37,83 +36,116 @@ import java.util.List;
 @Transactional
 public class UserControllerContractImpl implements UserControllerContact {
 
-    private final UserEntityService userEntityService;
-    private static final Logger logger= LoggerFactory.getLogger(UserControllerContractImpl.class);
+   private final UserEntityService userEntityService;
+   private static final Logger logger = LoggerFactory.getLogger(UserControllerContractImpl.class);
 
-    @Override
-    public RestResponse<UserDTO> register(UserSaveRequest userSaveRequest) {
-        RegularExpression.controlSaveRequest(userSaveRequest);
-        this.checkExistUser(userSaveRequest.username(),userSaveRequest.email());
-        User user= UserMapper.INSTANCE.convertToEntity(userSaveRequest);
-        user=this.userEntityService.save(user);
-        return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.USER_ADDED.getMessage(),true);
-    }
+   @Override
+   public RestResponse<UserDTO> register(UserSaveRequest userSaveRequest) throws Exception {
+       try {
+           RegularExpression.controlSaveRequest(userSaveRequest);
+           this.checkExistUser(userSaveRequest.username(), userSaveRequest.email());
+           User user = UserMapper.INSTANCE.convertToEntity(userSaveRequest);
+           user = this.userEntityService.save(user);
+           return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.USER_ADDED.getMessage(), true);
+       }catch (Exception error){
+           throw new Exception(error.getMessage());
+       }
+   }
 
-    @Override
-    public UserDTO updateUser(UserUpdateRequest userUpdateRequest) {
-        User user=this.userEntityService.findByIdWithControl(userUpdateRequest.id());
-        if (user.getStatus()==Status.DEACTIVE){
+   @Override
+   public RestResponse<UserDTO> updateUser(UserUpdateRequest userUpdateRequest) throws Exception {
+      try {
+         User user = this.userEntityService.findByIdWithControl(userUpdateRequest.id());
+         if (user.getStatus() == Status.DEACTIVE) {
             logger.warn("The user wanted update is not active! email: {}", userUpdateRequest.email());
             throw new UserIsNotActiveException(ErrorMessages.USER_IS_NOT_ACTIVE);
-        }
-        RegularExpression.controlUpdateRequest(userUpdateRequest);
-        UserMapper.INSTANCE.updateUser(user,userUpdateRequest);
-        return UserMapper.INSTANCE.convertToDTO(user);
-    }
+         }
+         RegularExpression.controlUpdateRequest(userUpdateRequest);
+         UserMapper.INSTANCE.updateUser(user, userUpdateRequest);
+         return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.USER_UPDATED.getMessage(), true);
+      } catch (Exception error) {
+         throw new Exception(error.getMessage());
+      }
+   }
 
-    @Override
-    public String deleteUser(Long id) {
-        User user= this.userEntityService.findByIdWithControl(id);
-        this.userEntityService.delete(user);
-        return Messages.USER_DELETED.getMessage();
-    }
+   @Override
+   public RestResponse<String> deleteUser(Long id) throws Exception {
+      try {
+         User user = this.userEntityService.findByIdWithControl(id);
+         this.userEntityService.delete(user);
+         return RestResponse.message(Messages.USER_DELETED.getMessage());
+      } catch (Exception error) {
+         throw new Exception(error.getMessage());
+      }
+   }
 
-    @Override
-    public List<UserDTO> findAll() {
-        List<User> userList= this.userEntityService.findAll()
-                .stream()
-                .filter(user -> user.getStatus() == Status.ACTIVE)
-                .toList();
-        return UserMapper.INSTANCE.convertToDTOs(userList);
-    }
+   @Override
+   public RestResponse<List<UserDTO>> findAll() throws Exception {
+       try {
+           List<User> userList = this.userEntityService.findAll()
+                   .stream()
+                   .filter(user -> user.getStatus() == Status.ACTIVE)
+                   .toList();
+           return RestResponse.result(UserMapper.INSTANCE.convertToDTOs(userList), Messages.USERS_LISTED.getMessage(), true);
+       }catch (Exception error){
+           throw new Exception(error.getMessage());
+       }
+   }
 
-    @Override
-    public UserDTO findById(Long id) {
-        User user=this.userEntityService.findByIdWithControl(id);
-        if (user.getStatus() == Status.DEACTIVE){
+   @Override
+   public RestResponse<UserDTO> findById(Long id) throws Exception {
+      try {
+         User user = this.userEntityService.findByIdWithControl(id);
+         if (user.getStatus() == Status.DEACTIVE) {
             logger.warn("User not active or do not found id: {}", id);
             throw new ItemNotFoundException(ErrorMessages.NOT_FOUND_USER);
-        }
-        return UserMapper.INSTANCE.convertToDTO(user);
-    }
+         }
+         return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.USER_LISTED.getMessage(), true);
+      } catch (Exception error) {
+         throw new Exception(error.getMessage());
+      }
+   }
 
-    @Override
-    public UserDTO active(Long id) {
-        User user= this.userEntityService.findByIdWithControl(id);
-        this.userEntityService.changeStatusToActive(user.getId());
-        return UserMapper.INSTANCE.convertToDTO(user);
-    }
+   @Override
+   public RestResponse<UserDTO> active(Long id) throws Exception {
+       try {
+           User user = this.userEntityService.findByIdWithControl(id);
+           this.userEntityService.changeStatusToActive(user.getId());
+           return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.SUCCESSFULLY_ACTIVE.getMessage(), true);
+       }catch (Exception error){
+           throw new Exception(error.getMessage());
+       }
+   }
 
-    @Override
-    public UserDTO deactive(Long id) {
-        User user= this.userEntityService.findByIdWithControl(id);
-        this.userEntityService.changeStatusToDeactive(user.getId());
-        return UserMapper.INSTANCE.convertToDTO(user);
-    }
+   @Override
+   public RestResponse<UserDTO> deactive(Long id) throws Exception {
+       try {
+           User user = this.userEntityService.findByIdWithControl(id);
+           this.userEntityService.changeStatusToDeactive(user.getId());
+           return RestResponse.result(UserMapper.INSTANCE.convertToDTO(user), Messages.SUCCESSFULLY_DEACTIVE.getMessage(), true);
+       }catch (Exception error){
+           throw new Exception(error.getMessage());
+       }
+   }
 
-    @Override
-    public List<UserDTO> findAllOfDeactives() {
-        List<User> userList= this.userEntityService.findAll()
-                .stream()
-                .filter(user -> user.getStatus() == Status.DEACTIVE)
-                .toList();
-        return UserMapper.INSTANCE.convertToDTOs(userList);
-    }
+   @Override
+   public RestResponse<List<UserDTO>> findAllOfDeactives() throws Exception {
+       try {
+           List<User> userList = this.userEntityService.findAll()
+                   .stream()
+                   .filter(user -> user.getStatus() == Status.DEACTIVE)
+                   .toList();
+           return RestResponse.result(UserMapper.INSTANCE.convertToDTOs(userList), Messages.USERS_LISTED.getMessage(), true);
+       }catch (Exception error){
+           throw new Exception(error.getMessage());
+       }
+   }
 
-    private void checkExistUser(String username, String email){
-        User user= this.userEntityService.findByUsernameAndEmail(username, email);
-        if (user!=null){
-            throw new AlreadyExistsException(ErrorMessages.ALREADY_EXCEPTION_USER);
-        }
-    }
+   private void checkExistUser(String username, String email) {
+      User user = this.userEntityService.findByUsernameAndEmail(username, email);
+      if (user != null) {
+          logger.warn(ErrorMessages.ALREADY_EXCEPTION_USER.getMessage());
+         throw new AlreadyExistsException(ErrorMessages.ALREADY_EXCEPTION_USER);
+      }
+   }
 }
